62747d8efab5f8b810ccfbd7210f510e7cd6af58
AreeSsha <168686735+AreeSsha@users.noreply.github.com>
2025-01-21 17:26:54 +0500

    website.js

---
+++ website.js
@@ -0,0 +1,4 @@
+// JavaScript for handling the button click event
+document.getElementById('clickButton').addEventListener('click', function() {
+    alert('Hello! This is a message from JavaScript.');
+});
